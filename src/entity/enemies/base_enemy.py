1439bddfa8360fa1dcf8509a8b9b4305c5912776
srite <me@srit.xyz>
2026-01-12 17:45:45 +0300

    update ig

--- src/entity/enemies/base_enemy.py
+++ src/entity/enemies/base_enemy.py
@@ -2,7 +2,14 @@ import arcade
 
 from entity.player import Player
 
+PathOrTexture = str | arcade.Path | bytes | arcade.Texture | None # type: ignore
+
 
 class BaseEnemy(arcade.Sprite):
+
+    def __init__(self, damage: int, movespeed: int, texture: PathOrTexture):
+        super().__init__(texture)
+        self.damage = damage
+        self.movespeed = movespeed
     def collision_with_player(self, player: Player):
-        player.hitpoints -= 1
+        player.hitpoints -= self.damage
